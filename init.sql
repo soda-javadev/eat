5bb20b8ccd8e5631fee20ac677d93dd020753445
andrianovv <andrianovv@>
2024-10-24 21:08:32 +0300

    EAT-3: Added liquibase, testcontainers, docker-compose, postgres, init sql, context test

---
+++ init.sql
@@ -0,0 +1,2 @@
+create user eat with password 'eat';
+GRANT CREATE ON DATABASE postgres TO eat;
---
+++ src/test/java/ru/sodajavadev/eat/TestConfig.java
@@ -0,0 +1,9 @@
+package ru.sodajavadev.eat;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfig {
+
+
+}
