61fe2120b3d18ee3b2d049e316a40239b69d3541
дмитрий <semikin.dimon@mail.ru>
2024-10-26 02:02:22 +0300

    EAT-6: add fk on event_template, update Event.java

--- src/main/java/ru/sodajavadev/eat/entity/Event.java
+++ src/main/java/ru/sodajavadev/eat/entity/Event.java
@@ -2,9 +2,12 @@ package ru.sodajavadev.eat.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -31,5 +34,9 @@ public class Event {
     @ColumnDefault("false")
     @Column(name = "successfully", nullable = false)
     private Boolean successfully = false;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "event_template_id", nullable = false)
+    private EventTemplate eventTemplate;
 
 }
