06e02f0039906ca4422d3d899d11649d4f27405b
дмитрий <semikin.dimon@mail.ru>
2024-10-27 21:43:49 +0300

    EAT-9: add create_table_guild_member.xml, add GuildMember.java, update changelog_0.0.1.xml, update clear.sql, update create_table_event.xml, update Event.java, update create_table_loot.xml, update Loot.java

--- src/main/java/ru/sodajavadev/eat/entity/GuildMember.java
+++ src/main/java/ru/sodajavadev/eat/entity/GuildMember.java
@@ -5,30 +5,24 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Getter
 @Setter
 @Entity
-@Table(name = "loot", schema = "eat")
-public class Loot {
+@Table(name = "guild_member", schema = "eat")
+public class GuildMember {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "name", nullable = false, length = 1000)
-    private String lootName;
+    @Column(name = "nickname", nullable = false, length = 1000)
+    private String nickname;
 
-    @Column(name = "cost", nullable = false)
-    private Integer lootCost;
+    @Column(name = "server", nullable = false, length = 1000)
+    private String server;
 
-    @ManyToMany(mappedBy = "loot")
-    private List<Event> events = new ArrayList<>();
 }
