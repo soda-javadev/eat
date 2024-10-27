251bfff910ecdc096a73e15bdf3c0670da1e9f86
дмитрий <semikin.dimon@mail.ru>
2024-10-27 17:40:23 +0300

    EAT-7: add create_table_loot.xml, add entity Loot.java

---
+++ src/main/java/ru/sodajavadev/eat/entity/Loot.java
@@ -0,0 +1,28 @@
+package ru.sodajavadev.eat.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "loot", schema = "eat")
+public class Loot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "loot_name", nullable = false, length = 1000)
+    private String lootName;
+
+    @Column(name = "loot_cost", nullable = false)
+    private Integer lootCost;
+
+}
