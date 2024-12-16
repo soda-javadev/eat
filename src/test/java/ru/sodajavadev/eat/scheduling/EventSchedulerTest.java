5a9a313b8efef9f0d8ee09c34903b9cd4fa61761
дмитрий <semikin.dimon@mail.ru>
2024-12-16 17:36:50 +0300

    EAT-12: fix comments

--- src/test/java/ru/sodajavadev/eat/scheduling/EventSchedulerTest.java
+++ src/test/java/ru/sodajavadev/eat/scheduling/EventSchedulerTest.java
@@ -1,25 +1,32 @@
 package ru.sodajavadev.eat.scheduling;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import ru.sodajavadev.eat.configuration.SchedulerConfig;
+import ru.sodajavadev.eat.service.EventService;
 
 import static org.awaitility.Awaitility.await;
-import static org.awaitility.Durations.ONE_MINUTE;
+import static org.awaitility.Durations.TWO_SECONDS;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.verify;
 
-@SpringJUnitConfig(SchedulerConfig.class)
+@SpringBootTest(properties = "scheduler.event=0/1 * * * * *")
 class EventSchedulerTest {
 
-    @MockBean
+    @Autowired
     private EventScheduler scheduler;
+    @MockBean
+    private EventService eventService;
 
     @Test
     void run() {
+        Mockito.spy(scheduler);
         await()
-                .atMost(ONE_MINUTE)
-                .untilAsserted(() -> verify(scheduler, atLeast(1)).run());
+                .atMost(TWO_SECONDS)
+                .untilAsserted(() -> verify(eventService, atLeast(1))
+                        .processEvent(any(), any(), any(), any()));
     }
 }
